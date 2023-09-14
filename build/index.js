1a8cefbaadb006039a1681150069bf09ee6c29de
Kevin Gilpin <kgilpin@gmail.com>
2023-09-14 17:24:35 -0400

    feat: Update exports

--- build/index.js
+++ build/index.js
@@ -13,7 +13,16 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.verbose = exports.executeCommand = exports.log = void 0;
+var log_1 = require("./log");
+Object.defineProperty(exports, "log", { enumerable: true, get: function () { return __importDefault(log_1).default; } });
 __exportStar(require("./log"), exports);
+var executeCommand_1 = require("./executeCommand");
+Object.defineProperty(exports, "executeCommand", { enumerable: true, get: function () { return __importDefault(executeCommand_1).default; } });
 __exportStar(require("./executeCommand"), exports);
-__exportStar(require("./verbose"), exports);
+var verbose_1 = require("./verbose");
+Object.defineProperty(exports, "verbose", { enumerable: true, get: function () { return __importDefault(verbose_1).default; } });
